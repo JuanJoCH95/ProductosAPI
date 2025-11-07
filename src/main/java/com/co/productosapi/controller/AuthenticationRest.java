033237c8bbe319339bd589c6eb7b5ba763934c1f
Juan José Cardona Henao <juancardonah1017@gmail.com>
2025-11-06 19:31:58 -0500

    Se cambia Basic Authentication por Cookies y se crea el login

--- src/main/java/com/co/productosapi/controller/AuthenticationRest.java
+++ src/main/java/com/co/productosapi/controller/AuthenticationRest.java
@@ -1,11 +1,20 @@
 package com.co.productosapi.controller;
 
+import com.co.productosapi.model.LoginRequest;
 import com.co.productosapi.model.Response;
 import com.co.productosapi.model.Usuarios;
 import com.co.productosapi.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
@@ -15,9 +24,23 @@ public class AuthenticationRest {
     @Autowired
     private AuthenticationService services;
 
-    @GetMapping("/login")
-    public String login() {
-        return "Login Exitoso";
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest, HttpServletRequest request) {
+        try {
+            UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(
+                    loginRequest.getUsername(), loginRequest.getPassword());
+
+            Authentication authentication = authenticationManager.authenticate(authRequest);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            HttpSession session = request.getSession(true);
+            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+            return new ResponseEntity<String>("Sesión iniciada exitosamente!", HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<String>("Credenciales no validas", HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @PostMapping("/register")
--- src/main/java/com/co/productosapi/security/SecurityConfig.java
+++ src/main/java/com/co/productosapi/security/SecurityConfig.java
@@ -2,34 +2,36 @@ package com.co.productosapi.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    private final UserDetailsService userDetailsService;
-
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Bean
     public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(authRequest -> authRequest
                 .requestMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
-        ).httpBasic(Customizer.withDefaults()).build();
+        ).exceptionHandling(exceptions -> exceptions
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))).build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
